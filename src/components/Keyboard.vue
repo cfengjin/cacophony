343c4b8e287db74b05a2a622b11995ebb3fa0933
Fengjin Cui <cfengjin@umich.edu>
2020-12-18 20:50:42 -0500

    Play chords with different waveforms and volume

--- src/components/Keyboard.vue
+++ src/components/Keyboard.vue
@@ -1,5 +1,6 @@
 <template>
   <Chart />
+
   <div class="keyboard">
     <div
       class="key"
@@ -9,6 +10,29 @@
       @click="pitch.selected = !pitch.selected"
     />
   </div>
+
+  <button @mousedown="play">Play</button>
+  <button @click="clear">Clear</button>
+
+  <br />
+  <label for="volume">Volume:</label>
+  <br />
+  <input type="range" id="volume" name="volume" min="0" max="1" step="0.01" v-model="volume">
+
+  <p>Select a waveform:</p>
+
+  <input type="radio" id="sine" name="waveform" value="sine" v-model="waveform">
+  <label for="sine">Sine</label>
+  <br />
+  <input type="radio" id="square" name="waveform" value="square" v-model="waveform">
+  <label for="square">Square</label>
+  <br />
+  <input type="radio" id="triangle" name="waveform" value="triangle" v-model="waveform">
+  <label for="triangle">Triangle</label>
+  <br />
+  <input type="radio" id="sawtooth" name="waveform" value="sawtooth" v-model="waveform">
+  <label for="sawtooth">Sawtooth</label>
+
 </template>
 
 <script>
@@ -20,10 +44,49 @@ export default {
   },
   data() {
     return {
+      audioContext: new (window.AudioContext || window.webkitAudioContext)(),
+      masterGainNode: {},
+      volume: 0.1,
       pitches: [],
+      waveform: 'sine',
+      playing: false
+    }
+  },
+  computed: {
+    selectedPitches() {
+      return this.pitches.filter(pitch => pitch.selected)
+    }
+  },
+  watch: {
+    volume() {
+      this.masterGainNode.gain.value = this.volume
+    }
+  },
+  methods: {
+    play() {
+      this.selectedPitches.forEach(pitch => {
+        pitch.oscillator = this.audioContext.createOscillator()
+        pitch.oscillator.frequency.value = pitch.frequency
+        pitch.oscillator.type = this.waveform
+        pitch.oscillator.connect(this.masterGainNode)
+        pitch.oscillator.start()
+      })
+      this.playing = true
+    },
+    stop() {
+      if (this.playing) {
+        this.selectedPitches.forEach(pitch => pitch.oscillator.stop())
+        this.playing = false
+      }
+    },
+    clear() {
+      this.selectedPitches.forEach(pitch => pitch.selected = false)
     }
   },
   created() {
+    this.masterGainNode = this.audioContext.createGain()
+    this.masterGainNode.connect(this.audioContext.destination)
+    this.masterGainNode.gain.value = this.volume
     let classes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
     let baseFreq = 27.5
     let numPitches = 88
@@ -32,10 +95,12 @@ export default {
         class: classes[i%12],
         frequency: baseFreq,
         selected: false,
+        oscillator: {} 
       })
       baseFreq *= 2 ** (1/12)
     }
-  }
+    window.addEventListener('mouseup', this.stop)
+  },
 }
 </script>
 
@@ -46,17 +111,23 @@ export default {
   overflow: auto;
 }
 .key {
-  width: 24px;
-  min-width: 24px;
-  height: 144px;
-  min-height: 144px;
+  margin: 0 2px;
+  width: 30px;
+  min-width: 30px;
+  height: 180px;
+  min-height: 180px;
   background-color: white;
-  border: 2px solid #1f1f1f;
+  border: 1px solid #1f1f1f;
 }
 .black {
+  width: 18px;
+  min-width: 18px;
+  height: 108px;
+  min-height: 108px;
   background-color: #1f1f1f;
 }
 .selected {
-  border: 2px solid lightskyblue;
+  background-color: deepskyblue;
+  border-color: deepskyblue;
 }
 </style>
