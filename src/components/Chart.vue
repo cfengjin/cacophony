fec6b95de970c8131fc36510225d58ed7c09c8a2
Fengjin Cui <cfengjin@umich.edu>
2020-12-29 21:02:26 -0500

    Zoom out a bit

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -76,7 +76,7 @@ export default {
     },
     y() {
       return d3.scaleLinear()
-        .domain([this.showInterference ? -this.pitches.length : -1, this.showInterference ? this.pitches.length : 1])
+        .domain([this.showInterference ? -this.pitches.length : -1.25, this.showInterference ? this.pitches.length : 1.25])
         .range([this.chart.height - 24, 24])
     },
     line() {
